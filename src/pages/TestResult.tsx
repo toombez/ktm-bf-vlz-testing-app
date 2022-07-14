2a8076ee5a5f14e6e7d57902bfe0a20b47d9ae6c
toombez <timurtokaev.toombez@gmail.com>
2022-07-14 20:32:08 +0300

    Getting lastResult into TestResult

--- src/pages/TestResult.tsx
+++ src/pages/TestResult.tsx
@@ -1,6 +1,41 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+import { store } from "../store/store";
+
 const TestResult = () => {
+    const [result] = useState(store.getState().testsStore.lastResult);
+
+    const questionsCount = result?.test.questions.length;
+
+    const rightQuestionsCount = result?.testQuestionsData
+        /**
+         * TODO
+         * use classes here
+         */
+        .reduce((count, testQuestionData, index) => {
+            const rightAnswers = testQuestionData.question.answers
+                .filter(answer => answer.isRight);
+            const selectedRightAnswers = testQuestionData.answers
+                .filter(answer => rightAnswers.includes(answer));
+
+            const haveMistake = rightAnswers.length === selectedRightAnswers.length;
+            const selectAnswersCountEqualsRight = rightAnswers.length === testQuestionData.answers.length;
+
+            return haveMistake && selectAnswersCountEqualsRight ? count + 1 : count;
+        }, 0);
+
     return (
         <div>
+            <Link to="/">На главную</Link>
+            <div>
+                <h2>
+                    { result?.test.title }
+                </h2>
+                <div>
+                    Результат <span>{rightQuestionsCount}</span> из <span>{questionsCount}</span>
+                </div>
+            </div>
+            
         </div>
     )
 }
