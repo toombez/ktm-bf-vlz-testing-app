d3022b9a1a13847b132231349f46b63e3c703adb
Timur <81884993+toombez@users.noreply.github.com>
2022-07-14 18:00:04 +0300

    Feat/select test page (#3)
    * Add TestCard
    
    * Refactor SelectTest page

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Route, Routes } from 'react-router-dom';
-import TestSelector from '../components/TestSelector';
-import { fetchTests } from '../store/reducers/tests';
-import { store } from '../store/store';
-import Testing from './Testing';
+import { fetchTests } from './store/reducers/tests';
+import { store } from './store/store';
+import Testing from './pages/Testing';
+import SelectTest from './pages/SelectTest'
 
 function App() {
     store.dispatch(fetchTests())
@@ -10,7 +10,7 @@ function App() {
     return (
         <div className="App">
             <Routes>
-                <Route path='/' element={<TestSelector />} />
+                <Route path='/' element={<SelectTest />} />
                 <Route path="/test/:id" element={<Testing />} />
             </Routes>
         </div>
